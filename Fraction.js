55183d1cb98cc23b5b2732e16040b7d605be3f6f
MAZ <MAZ01001@users.noreply.github.com>
2021-04-30 03:52:40 +0200

    Update Fraction.js
    fraction from decimal function

--- Fraction.js
+++ Fraction.js
@@ -1,9 +1,9 @@
 class Fraction{
     /**
      * __creates a (mixed) Fraction__
-     * @param {number} a _(Real-Integer)_ "whole number" - _default `0`_
-     * @param {number} b _(Real-Integer)_ "numerator" - _default `0`_
-     * @param {number} c _(Real-Integer)_ "denominator" - _default `1`_
+     * @param {number} a _(Real-Integer)_ "whole number" - _default `0` (can be negative)_
+     * @param {number} b _(Real-Integer)_ "numerator" - _default `0` (can be negative)_
+     * @param {number} c _(Real-Integer)_ "denominator" - _default `1` (can be negative)_
      * @throws Error if denominator is `0`
      * @description formula: `(a+(b/c))` (mixed-fraction)
      */
@@ -73,10 +73,44 @@ class Fraction{
         }
         return this;
     }
+    /**
+     * __converting a decimal number to a Fraction__
+     * @param {number} v - _Real_ decimal number _(auto detects repeating digits) - default `0.0` (can be negative)_
+     * @param {boolean} nl - if `true` doesn't loop -> literal `0.4` not `0.44444..4`
+     * @returns {Fraction} Fraction-Object
+     */
+    static fromdecimal(v=0.0,nl=false){
+        if(v==0.0){return new Fraction();}
+        let _az,f=new Fraction();
+        if(v<0){f.neg=true;}
+        [,f.a,_az]=[...Math.abs(v).toString().match(/(-?[0-9]+)(?:[,.]([0-9]+))?/)];
+        f.a=parseInt(f.a);
+        if(!_az){_az='0';}
+        if(nl){
+            f.b=parseInt(_az);
+            f.c=Math.pow(10,_az.length);
+        }else{
+            const r=/^((?<d>[0-9]+?)\k<d>*)$/;
+            const _a=_az.match(r)[1],
+                _d=_az.match(r)[2];
+            f.b=parseInt(_d);
+            f.c=Math.pow(10,_d.length);
+            if(_a!=_d||_a.length==1){f.c--;}
+        }
+        return f.toMixed();
+    }
+    /**
+     * __computes the decimal value of the fraction and returns it__
+     * @returns {number} decimal value of the fraction
+     */
+    todecimal(){return (this.neg?-this.a:this.a)+(this.b/this.c);}
+    /**
+     * __creates a formated string of the fraction with decimal value__
+     * @returns {string} - `+-(a+(b/c)) ~ +-1.234`
+     */
+    tostr(){return `${this.neg?'-':'+'}(${this.a}+(${this.b}/${this.c})) ~ ${this.todecimal()}`;}
     // TODO documentation
     // TODO neg-numbers !
-    computeValue(){return this.a+(this.b/this.c);}
-    tostr(){return `${this.a}+(${this.b}/${this.c}) ~ ${this.computeValue()}`;}
     add_n(n=0){
         if(n==0){return this;}
         this.a+=n;
@@ -131,7 +165,10 @@ class Fraction{
         return this;
     }
 };
-// TODO
 console.log(
-    new Fraction(0,1,3).div_f(new Fraction(0,3,4)).tostr()//=> 4/9 ~ 0.444..
+    // new Fraction(0,1,3).div_f(new Fraction(0,3,4)).tostr()//=> 4/9 ~ 0.444..
+    // Fraction.fromdecimal(3.4).tostr()//=> 3 4/9 ~ 3.44444..6
+    // Fraction.fromdecimal(3.445).tostr()//=> 3 89/200 ~ 3.445
+    // Fraction.fromdecimal(3.44,true).tostr()//=> 3 11/25 ~ 3.44
+    Fraction.fromdecimal(3.3).tostr()//=> 3 1/3 ~ 3.33333..5
 );
